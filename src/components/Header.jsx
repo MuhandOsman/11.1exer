c20a984451477efcf5bb5ce964ed91f0d1b0aa52
MuhandOsman <mo1981ot@gmail.com>
2021-11-02 20:22:04 +0100

    img added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
 import React, {useContext} from 'react'
 import MyContext from '../context/MyContext'
-
+import chucky from "../assets/chucky.jpg"
 const Header = () => {
     const shop = useContext(MyContext)
     const {categories,select, setSelect} = shop
 
     return (
         <div className="header" >
+        
            <label >CHOOSE A CATEGORY</label>
+           <img src={chucky} alt="" />
             <select defaultValue={select} onChange={(e)=> setSelect(e.target.value) } >
                 <option value="default"  >choose a category</option>
                 {categories.map((category,index) => <option key={index} value={category}>{category}</option> )}   
